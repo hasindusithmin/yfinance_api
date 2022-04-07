edeaa2fce985ddf395e5dbeb4ecde9bfeb4c9d63
hasindu sithmin <hasindusithmin64@gmail.com>
2022-04-07 17:29:02 +0000

    Uvicorn debug mode

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import uvicorn
 from fastapi import FastAPI
 
 description = """
@@ -23,8 +23,12 @@ app = FastAPI(
 
 @app.get("/")
 def root():
-    return "Hello World"
+    return "HelloWorld"
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app, 
+        host="0.0.0.0", 
+        port=8000
+    )
