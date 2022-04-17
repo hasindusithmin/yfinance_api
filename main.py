238cc9589505fb4b5a317558499d4554f31e5ffa
hasindu sithmin <hasindusithmin64@gmail.com>
2022-04-17 17:08:46 +0000

    add new routes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 from functions.myfn import downloader,converter
-
+import yfinance as yf
 
 description = """
-    https://api.domain.com/{ticker}/{interval}
-    # valid ticker:EURUSD,AUDUSD,GBPUSD,NZDUSD,EURJPY,GBPJPY,EURGBP,EURCAD,EURSEK...()
+    # valid ticker:https://yfinanceapi.hasindusithmin.repl.co
     # valid intervals:5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
 """
 
@@ -22,11 +21,18 @@ app = FastAPI(
     },
 )
 
+names = ['EUR/USD', 'USD/JPY', 'GBP/USD', 'AUD/USD', 'NZD/USD', 'EUR/JPY', 'GBP/JPY', 'EUR/GBP', 'EUR/CAD', 'EUR/SEK', 'EUR/CHF', 'EUR/HUF', 'EUR/JPY', 'USD/CNY', 'USD/HKD', 'USD/SGD', 'USD/INR', 'USD/MXN', 'USD/PHP', 'USD/IDR', 'USD/THB', 'USD/MYR', 'USD/ZAR', 'USD/RUB']
+symbol= ['EURUSD=X', 'JPY=X', 'GBPUSD=X', 'AUDUSD=X', 'NZDUSD=X', 'EURJPY=X', 'GBPJPY=X', 'EURGBP=X', 'EURCAD=X', 'EURSEK=X', 'EURCHF=X', 'EURHUF=X', 'EURJPY=X', 'CNY=X', 'HKD=X', 'SGD=X', 'INR=X', 'MXN=X', 'PHP=X', 'IDR=X', 'THB=X', 'MYR=X', 'ZAR=X', 'RUB=X']
+
 @app.get("/")
 def root():
-    return "welcome to finance api"
+    return list(
+        zip(
+            names,symbol
+        )
+    )
 
-@app.get("/forex/{currency}/{interval}")
+@app.get("/data/{currency}/{interval}")
 def forex(currency:str,interval:str):
     return converter(
         downloader(
@@ -34,7 +40,10 @@ def forex(currency:str,interval:str):
             interval=interval
         )
     )
-    
+
+@app.get("/news/{currency}")
+def get_news(currency:str):
+    return yf.Ticker(currency).news
 
 if __name__ == "__main__":
     uvicorn.run(
